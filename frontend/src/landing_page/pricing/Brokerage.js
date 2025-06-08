213f1f80c945b2a6bb975f1d0df2692a43f92e21
ghanashyam2003 <shubhambadgujar2003@gmail.com>
2025-06-08 23:09:24 +0530

    Update Footer

--- frontend/src/landing_page/pricing/Brokerage.js
+++ frontend/src/landing_page/pricing/Brokerage.js
@@ -3,23 +3,30 @@ import React from 'react'
 function Brokerage() {
   return (
     <div className="container">
-      <div className="row p-5 mt-5 text-center">
-        
-      </div>
-      <div
-        className="row p-5 mt-5 fs-6 text-center"
-        style={{ lineHeight: "1.7" }}
-      >
-        <div className="col-4 p-3">
-          
+      <div className="row p-5 mt-5 text-center border-top">
+        <div className="col-8 p-4">
+        <a href='' style={{textDecoration:"None"}}>  <h3 className='fs-5'>Brokerage calculator</h3></a>
+        <ul className='text-start fs-6 text-muted ' style={{lineHeight:"2.5"}}>
+          <li>
+            <a href='' style={{textDecoration:"None"}}>Equity delivery</a> - ₹ 0
+          </li>
+          <li>
+            <a href='' style={{textDecoration:"None"}}>Intraday and F&O trades</a> - ₹ 20 or 0.03% (whichever is lower)
+          </li>
+          <li>
+            <a href='' style={{textDecoration:"None"}}>Options trades</a> - ₹ 20 per executed order
+          </li>
+          <li>
+            <a href='' style={{textDecoration:"None"}}>Currency and commodity trades</a> - ₹ 20 or 0.03% (whichever is lower)
+          </li>
+        </ul>
         </div>
-        <div className="col-4 p-3">
-          
+        <div className="col-4 p-4">
+        <a href='' style={{textDecoration:"None"}}>  <h3 className='fs-5'>List of charges</h3></a>
         </div>
-        
       </div>
     </div>
-  )
+  );
 }
 
 export default Brokerage
--- frontend/src/landing_page/support/CreateTicket.js
+++ frontend/src/landing_page/support/CreateTicket.js
@@ -1,9 +1,233 @@
-import React from 'react'
+import React from "react";
 
 function CreateTicket() {
   return (
-    <h1>CreateTicket</h1>
-  )
+    <div className="container">
+      <div className="row p-5 mt-5 mb-5">
+        <h3 className=" mb-4 fs-4">
+          To create a ticket, select a relevant topic
+        </h3>
+
+        <div className="col-4 p-4" style={{ lineHeight: "2.5" }}>
+          <h4 className="mb-3">
+            <i className="fa-solid fa-circle-plus me-2"></i>
+            Account Opening
+          </h4>
+
+          <ul className="list-unstyled">
+            <li>
+              <a href="#" className="text-decoration-none ">
+                Resident individual
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-decoration-none ">
+                Minor
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-decoration-none ">
+                Non Resident Indian (NRI)
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-decoration-none ">
+                Company, Partnership, HUF and LLP
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-decoration-none ">
+                Glossary
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="col-4 p-4" style={{ lineHeight: "2.5" }}>
+          <h4 className="mb-3">
+            <i class="fa-regular fa-user"></i> Your Zerodha Account
+          </h4>
+
+          <ul className="list-unstyled">
+            <li>
+              <a href="#" className="text-decoration-none ">
+                Your Profile
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-decoration-none ">
+                Account modification
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-decoration-none ">
+                Client Master Report (CMR) and Depository Participant (DP)
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-decoration-none ">
+                Nomination
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-decoration-none ">
+                Transfer and conversion of securities
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="col-4 p-4" style={{ lineHeight: "2.5" }}>
+          <h4 className="mb-3">
+            <i class="fa-solid fa-chart-column"></i> Kite
+          </h4>
+
+          <ul className="list-unstyled">
+            <li>
+              <a href="#" className="text-decoration-none ">
+                {" "}
+                IPO
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-decoration-none ">
+                {" "}
+                Trading FAQs{" "}
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-decoration-none ">
+                {" "}
+                Margin Trading Facility (MTF) and Margins{" "}
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-decoration-none ">
+                {" "}
+                Charts and orders{" "}
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-decoration-none ">
+                {" "}
+                Alerts and Nudges{" "}
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-decoration-none ">
+                {" "}
+                General{" "}
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="col-4 p-4" style={{ lineHeight: "2.5" }}>
+          <h4 className="mb-3">
+            <i class="fa-solid fa-folder-closed"></i> Funds
+          </h4>
+
+          <ul className="list-unstyled">
+            <li>
+              <a href="#" className="text-decoration-none ">
+                Add money
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-decoration-none ">
+                Withdraw money
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-decoration-none ">
+                Add bank accounts
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-decoration-none ">
+                eMandates
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="col-4 p-4" style={{ lineHeight: "2.5" }}>
+          <h4 className="mb-3">
+            <i class="fa-solid fa-circle-user"></i> Console
+          </h4>
+
+          <ul className="list-unstyled">
+            <li>
+              <a href="#" className="text-decoration-none ">
+               Portfolio
+
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-decoration-none ">
+                Corporate actions
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-decoration-none ">
+               Funds statement
+
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-decoration-none ">
+                Reports
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-decoration-none ">
+                Profile
+
+              </a>
+            </li>
+             <li>
+              <a href="#" className="text-decoration-none ">
+                Segments
+
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="col-4 p-4" style={{ lineHeight: "2.5" }}>
+          <h4 className="mb-3">
+            <i class="fa-solid fa-circle-dot"></i> Coin
+          </h4>
+
+          <ul className="list-unstyled">
+            <li>
+              <a href="#" className="text-decoration-none ">
+                Mutual funds
+
+
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-decoration-none ">
+                National Pension Scheme (NPS)
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-decoration-none ">
+                Features on Coin
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-decoration-none ">
+                Payments and Orders
+
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-decoration-none ">
+                General
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default CreateTicket
+export default CreateTicket;
