6597c112221ec7ba4c363098df292561e5977332
ghanashyam2003 <shubhambadgujar2003@gmail.com>
2025-06-08 23:16:19 +0530

    Update the Footer margin

--- frontend/src/landing_page/Footer.js
+++ frontend/src/landing_page/Footer.js
@@ -51,37 +51,37 @@ function Footer() {
                 </a>
               </li>
               <li>
-                <a href="#" className="text-dark text-decoration-none text-muted">
+                <a href="#" className="text-dark text-decoration-none text-muted" style={{lineHeight: "2.5"}}>
                   Minor demat account
                 </a>
               </li>
               <li>
-                <a href="#" className="text-dark text-decoration-none text-muted">
+                <a href="#" className="text-dark text-decoration-none text-muted" style={{lineHeight: "2.5"}}>
                   NRI demat account
                 </a>
               </li>
               <li>
-                <a href="#" className="text-dark text-decoration-none text-muted">
+                <a href="#" className="text-dark text-decoration-none text-muted" style={{lineHeight: "2.5"}}>
                   Commodity
                 </a>
               </li>
               <li>
-                <a href="#" className="text-dark text-decoration-none text-muted">
+                <a href="#" className="text-dark text-decoration-none text-muted" style={{lineHeight: "2.5"}}>
                   Dematerialisation
                 </a>
               </li>
               <li>
-                <a href="#" className="text-dark text-decoration-none text-muted">
+                <a href="#" className="text-dark text-decoration-none text-muted" style={{lineHeight: "2.5"}}>
                   Fund transfer
                 </a>
               </li>
               <li>
-                <a href="#" className="text-dark text-decoration-none text-muted">
+                <a href="#" className="text-dark text-decoration-none text-muted" style={{lineHeight: "2.5"}}>
                   MTF
                 </a>
               </li>
               <li>
-                <a href="#" className="text-dark text-decoration-none text-muted">
+                <a href="#" className="text-dark text-decoration-none text-muted" style={{lineHeight: "2.5"}}>
                   Referral program
                 </a>
               </li>
@@ -93,42 +93,42 @@ function Footer() {
             <h6 className="fw-semibold text-dark">Support</h6>
             <ul className="list-unstyled">
               <li>
-                <a href="#" className="text-dark text-decoration-none text-muted">
+                <a href="#" className="text-dark text-decoration-none text-muted" style={{lineHeight: "2.5"}}>
                   Contact us
                 </a>
               </li>
               <li>
-                <a href="#" className="text-dark text-decoration-none text-muted">
+                <a href="#" className="text-dark text-decoration-none text-muted" style={{lineHeight: "2.5"}}>
                   Support portal
                 </a>
               </li>
               <li>
-                <a href="#" className="text-dark text-decoration-none text-muted">
+                <a href="#" className="text-dark text-decoration-none text-muted" style={{lineHeight: "2.5"}}>
                   How to file a complaint?
                 </a>
               </li>
               <li>
-                <a href="#" className="text-dark text-decoration-none text-muted">
+                <a href="#" className="text-dark text-decoration-none text-muted" style={{lineHeight: "2.5"}}>
                   Status of your complaints
                 </a>
               </li>
               <li>
-                <a href="#" className="text-dark text-decoration-none text-muted">
+                <a href="#" className="text-dark text-decoration-none text-muted" style={{lineHeight: "2.5"}}>
                   Bulletin
                 </a>
               </li>
               <li>
-                <a href="#" className="text-dark text-decoration-none text-muted">
+                <a href="#" className="text-dark text-decoration-none text-muted" style={{lineHeight: "2.5"}}>
                   Circulars
                 </a>
               </li>
               <li>
-                <a href="#" className="text-dark text-decoration-none text-muted">
+                <a href="#" className="text-dark text-decoration-none text-muted" style={{lineHeight: "2.5"}}>
                   Z-Connect blog
                 </a>
               </li>
               <li>
-                <a href="#" className="text-dark text-decoration-none text-muted">
+                <a href="#" className="text-dark text-decoration-none text-muted" style={{lineHeight: "2.5"}}>
                   Downloads
                 </a>
               </li>
@@ -140,37 +140,37 @@ function Footer() {
             <h6 className="fw-semibold text-dark">Company</h6>
             <ul className="list-unstyled">
               <li>
-                <a href="#" className="text-dark text-decoration-none text-muted">
+                <a href="#" className="text-dark text-decoration-none text-muted" style={{lineHeight: "2.5"}}>
                   About
                 </a>
               </li>
               <li>
-                <a href="#" className="text-dark text-decoration-none text-muted">
+                <a href="#" className="text-dark text-decoration-none text-muted" style={{lineHeight: "2.5"}}>
                   Philosophy
                 </a>
               </li>
               <li>
-                <a href="#" className="text-dark text-decoration-none text-muted">
+                <a href="#" className="text-dark text-decoration-none text-muted" style={{lineHeight: "2.5"}}>
                   Press & media
                 </a>
               </li>
               <li>
-                <a href="#" className="text-dark text-decoration-none text-muted">
+                <a href="#" className="text-dark text-decoration-none text-muted" style={{lineHeight: "2.5"}}>
                   Careers
                 </a>
               </li>
               <li>
-                <a href="#" className="text-dark text-decoration-none text-muted">
+                <a href="#" className="text-dark text-decoration-none text-muted" style={{lineHeight: "2.5"}}>
                   Zerodha Cares (CSR)
                 </a>
               </li>
               <li>
-                <a href="#" className="text-dark text-decoration-none text-muted">
+                <a href="#" className="text-dark text-decoration-none text-muted" style={{lineHeight: "2.5"}}>
                   Zerodha tech
                 </a>
               </li>
               <li>
-                <a href="#" className="text-dark text-decoration-none text-muted">
+                <a href="#" className="text-dark text-decoration-none text-muted" style={{lineHeight: "2.5"}}>
                   Open source
                 </a>
               </li>
@@ -182,37 +182,37 @@ function Footer() {
             <h6 className="fw-semibold text-dark">Quick Links</h6>
             <ul className="list-unstyled">
               <li>
-                <a href="#" className="text-dark text-decoration-none text-muted">
+                <a href="#" className="text-dark text-decoration-none text-muted" style={{lineHeight: "2.5"}}>
                   Upcoming IPO
                 </a>
               </li>
               <li>
-                <a href="#" className="text-dark text-decoration-none text-muted">
+                <a href="#" className="text-dark text-decoration-none text-muted" style={{lineHeight: "2.5"}}>
                   Brokerage charges
                 </a>
               </li>
               <li>
-                <a href="#" className="text-dark text-decoration-none text-muted">
+                <a href="#" className="text-dark text-decoration-none text-muted" style={{lineHeight: "2.5"}}>
                   Market holidays
                 </a>
               </li>
               <li>
-                <a href="#" className="text-dark text-decoration-none text-muted">
+                <a href="#" className="text-dark text-decoration-none text-muted" style={{lineHeight: "2.5"}}>
                   Economic calendar
                 </a>
               </li>
               <li>
-                <a href="#" className="text-dark text-decoration-none text-muted">
+                <a href="#" className="text-dark text-decoration-none text-muted" style={{lineHeight: "2.5"}}>
                   Calculators
                 </a>
               </li>
               <li>
-                <a href="#" className="text-dark text-decoration-none text-muted">
+                <a href="#" className="text-dark text-decoration-none text-muted" style={{lineHeight: "2.5"}}>
                   Markets
                 </a>
               </li>
               <li>
-                <a href="#" className="text-dark text-decoration-none text-muted">
+                <a href="#" className="text-dark text-decoration-none text-muted" style={{lineHeight: "2.5"}}>
                   Sectors
                 </a>
               </li>
