a100c9b3e96fcc0885bd5731590e5d43e07eb9ea
ghanashyam2003 <shubhambadgujar2003@gmail.com>
2025-06-08 23:41:38 +0530

    Work on Support Pricing section

--- frontend/src/landing_page/support/hero.js
+++ frontend/src/landing_page/support/hero.js
@@ -1,7 +1,26 @@
 import React from "react";
 
 function Hero() {
-  return <h1>Hero</h1>;
+  return (
+    <section className="container-fluid" id="supportHero">
+      <div className="row mt-5 mb-5 p-5" id ="supportWrapper">
+        <h4>
+          Support portal 
+          </h4>
+          <a href="" >Track Tickets</a>
+        
+      </div>
+      <div className="row mt-5 mb-5 p-5">
+        <div className="col-6 mt-5 mb-5 p-5">   </div>
+
+        <h4>
+          Support portal 
+          </h4>
+          <a href="" >Trick Ticket</a>
+        
+      </div>
+    </section>
+  );
 }
 
 export default Hero;
