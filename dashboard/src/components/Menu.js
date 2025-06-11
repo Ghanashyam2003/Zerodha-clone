1b7e49e3b710865ca7ac7666a49060e72464aa0a
ghanashyam2003 <shubhambadgujar2003@gmail.com>
2025-06-11 23:46:49 +0530

    Update the Menu file

--- dashboard/src/components/Menu.js
+++ dashboard/src/components/Menu.js
@@ -87,12 +87,7 @@ const Menu = () => {
                 Apps
               </p>
             </Link>
-          </li>
-        </ul>
-        <hr />
-        <div className="profile" onClick={handleProfileClick}>
-          <div className="avatar">ZU</div>
-          <p className="username">USERID</p>
+         
         </div>
       </div>
     </div>
